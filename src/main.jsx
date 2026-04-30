c14fa7475d9e063afd43dd71eca47effe59835f4
FhdQtr <fhd.alqahtani@gmail.com>
2026-04-30 14:51:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import ArenaCreate from "./ArenaCreate.jsx";
+import ArenaPlayer from "./ArenaPlayer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/arena/create" element={<ArenaCreate />} />
+        <Route path="/arena/join" element={<ArenaPlayer />} />
+        <Route path="/arena/join/:code" element={<ArenaPlayer />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
